98a9dec7d15faac4d389d5dcfcd1248ecb6a7f23
bluesstick <69700617+bluesstick@users.noreply.github.com>
2020-09-24 19:29:41 -0400

    Organizing my repo

---
+++ Chap2/TryItOut1Chap2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryItOut1Chap2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("The first app in Beginning Visual C# 2012!");
+            Console.ReadKey();
+        }
+    }
+}
