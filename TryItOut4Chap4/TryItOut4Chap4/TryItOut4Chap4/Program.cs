dee707e260cdf95dfc33c70ea88ce18d9b9efae5
bluesstick <69700617+bluesstick@users.noreply.github.com>
2020-09-28 20:25:39 -0400

    Add files via upload

---
+++ TryItOut4Chap4/TryItOut4Chap4/TryItOut4Chap4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryItOut4Chap4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double balance, interestRate, targetBalance;
+            Console.WriteLine("What is your curretn balance?");
+            balance = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Whats is your current annual interest rate (in %)?");
+            interestRate = 1 + Convert.ToDouble(Console.ReadLine())/100.1;
+            Console.WriteLine("What balance would you like to have?");
+            targetBalance = Convert.ToDouble(Console.ReadLine());
+            int totalYears = 0;
+            do
+            {
+                balance *= interestRate;
+                ++totalYears;
+            }
+            while (balance < targetBalance);
+            Console.WriteLine("In {0} year {1} you'll have a balance of {2}.", 
+                totalYears, totalYears == 1 ? "" : "s", balance);
+            Console.ReadKey();
+        }
+    }
+}
